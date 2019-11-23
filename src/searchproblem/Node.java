1798b16f31ea55a54694113c3602bb7977d71910
Kyle Perry <perry.246@gmail.com>
2019-11-23 11:39:34 -0700

    Added some base functionality to the nodes: 	- compareTo function for sorting nodes by penalty in ascending order 	- constructors, and getters 	- dummied out function to create children of the node 	(work in progress)

--- src/searchproblem/Node.java
+++ src/searchproblem/Node.java
@@ -1,10 +1,71 @@
 package searchproblem;
 import java.util.ArrayList;
 
-import searchproblem.classes.Slot;
+import searchproblem.classes.*;
 
-public class Node{
-        Slot assigned_slot;
-        ArrayList<Node> children;
-        Node parent;    // needed to backtrack if all children violate hard constraints
+public class Node implements Comparable<Node>{
+        private Slot assigned_slot;
+        private Course course = null;	//NULL if a lab is scheduled
+        private Lab lab = null;		//NULL if a course is scheduled
+        public int penalty;	//Penalty accrued from this course/lab assigned to assigned_slot
+        public  ArrayList<Node> children;
+        public Node parent;    // needed to backtrack if all children violate hard constraints
+        
+        public Node(Slot assigned_slot, Course course, Node parent){
+        	this.assigned_slot = assigned_slot;
+        	this.course = course;
+        	this.parent = parent;
+        	this.children = new ArrayList<Node>();
+        	this.penalty = 0;
+        	}
+        
+        public Node(Slot assigned_slot, Lab lab, Node parent){
+        	this.assigned_slot = assigned_slot;
+        	this.lab = lab;
+        	this.parent = parent;
+        	this.children = new ArrayList<Node>();
+        	this.penalty = 0;
+        }
+        
+        /*
+        	GETTERS BEGIN
+        */
+        public Slot getSlot() {
+        	return assigned_slot;
+        }
+        
+        //Will return null if a course was assigned
+        public Lab getLab() {
+        	return lab;
+        }
+        
+        //Will return null if a lab was assigned
+        public Course getCourse() {
+        	return course;
+        }
+        public ArrayList<Node> getChildren(){
+        	return children;
+        }
+        /*
+        	GETTERS END
+        */
+        
+        public int compareTo(Node other_assignment)
+        {
+        	if(this.penalty == other_assignment.penalty)
+        		return 0;
+        	else if (this.penalty > other_assignment.penalty)
+        		return 1;
+        	else
+        		return -1;
+        }
+        
+        public void generateChildren(ArrayList<Course> unassigned_courses, ArrayList<Lab> unassigned_labs, ArrayList<Slot> slots)
+        {
+        	//Using parent, gather list of previous assignments
+        	//For each course and lab in given lists, if it wasn't assigned before (in our lists we just generated, assign it to each slot of its type in a new node
+        	//add these new nodes to the children
+        	
+        	
+        }
     }
