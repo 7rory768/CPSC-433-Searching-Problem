7653296fec29be297542d4ed869eec5688128b84
Chandler Bullock <ckbullock410@gmail.com>
2019-11-27 00:02:53 -0700

    got some base code down for the Solver

--- src/searchproblem/Solver.java
+++ src/searchproblem/Solver.java
@@ -5,66 +5,120 @@ import java.util.ArrayList;
 public class Solver{
 	
 	private final Parser parser;
+    private final Evaluator evaluator;
+    private final Validator validator;
+    private int minPenalty = 0;
 	
 	public Solver(Parser parser) {
 		this.parser = parser;
+        this.evaluator = new Evaluator(parser);
+        this.validator = new Validator(parser);
 	}
 
     // gonna have to take a lot of params that the parser produces
     public void solve(){
         Node root = new Node();
         Node currentNode = root;
-        int minPenalty = 0;
 
-        ArrayList<Node> solution = new ArrayList<Node>();
         ArrayList<Node> bestSolution = new ArrayList<Node>();
-        /*  this will use Evaluator, and Evaluator to build out the solution
-	
-	solution = depthFirstSolve(solution);	//probably return an empty list if no solution exists, check if empty after calling this\
-	bestSolution = solution; //copy into here
-	
-	//while bestSolution isn't empty:
-		bestSolution = breadthFirstSolve(new ArrayList<Node>);
 
+        int solutionLength = parser.getCourses().size() + parser.getLabs().size();
 
-    ----- DEPTH FIRST SEARCH FIRST ---- find any solution, then do breadth
-	----- Will also want to do any partial assignments, or course Preferreds first, give us 
-	----- our best solutions first so Eval can eliminate as many branches as possible later
-        for every course:
-	    boolean validCombination = false
-            for every slot:
-                if solution + this course to this slot is Validated add to solution
-                    should probably split off into another function and recurse, and have it return
-                    a boolean of whether this path is valid if not continue to next slot
-
-		    validCombination = true
-	    if validCombination: break
-	if !validCombination: need to backtrack to next slot for last course assigned.
+        ArrayList<ScheduledItem> toBeScheduled = parser.getCourses().addAll(parser.getLabs());
         
-        ---- BREADTH SEARCH ---- use Eval in here
-        for every course:
-            for every slot:
-                push Eval onto ordered list of best assignments
-            search down the lowest penalty branch first .. aka assign it in solution
-            again recursion probs best for this
+        // TO-DO Need to decide the data structure that stores a partial assignment
+        ArrayList<Pair> partialAssignments = parser.getPartialAssignments();
+        ArrayList<Node> partialSolution;
+        int currentPenalty = 0;
+        
+        for(Pair pa : partialAssignments){
+            Node newNode = new Node();
+            //assign newNodes Slot and Course/Lab
+            // assign newNodes penalty with Eval, add to minPenalty
+            // check if valid .. if not then hard constraints contradict each other .. return error msg
+            partialSolution.add(newNode)
+        }
 
-        */
+	
+	    bestSolution = depthFirstSolve(partialSolution, toBeScheduled, solutionLength);	// return an empty list if no solution exists, check if empty after calling this\
+        if(bestSolution.size() == 0){
+            System.out.println("No solution found for this problem.");
+            System.exit(0);
+        }
+        minPenalty = evaluator.evaluate(bestSolution);  //probably more efficient ways to get this
+		bestSolution = breadthFirstSolve(partialSolution, toBeScheduled, minPenalty, solutionLength);
+        minPenalty = evaluator.evaluate(bestSolution);
+        System.out.println("Found an assignment with penalty of: " + Integer.toString(minPenalty));
     }
 
-    /*
-    private ArrayList<Node> depthFirstSolve(ArrayList<Node> solution, ArrayList<ScheduledItem> courseOrLab, int expectedPairsReturned){
-    // basically don't use eval for this one, just want any solution
-       for every slot
-            if can assign
-                Node newNode = new Node()
-                solution.append(newNode);
-                solution.append(depthFirstSolve(solution, nextCourse, expectedPairsReturned-1) //nextCourse probs just and index of some global ArrayList
-                if solution.length == expectedPairsReturned:
-                    return solution
-                else
-                    solution.remove(newNode)
-                    continue
-       return new ArrayList<Node>   // return empty list cause no valid solution down this branch
+    
+    private ArrayList<Node> depthFirstSolve(ArrayList<Node> solution, ArrayList<ScheduledItem> toBeScheduled){
+        // basically don't use eval for this one, just want any solution
+
+        ScheduledItem current = toBeScheduled.get(0);
+        toBeScheduled.remove(0);
+
+        Node newNode = new Node(current, solution.get(solution.size()-1));
+
+        ArrayList<Slot> slots = (newNode.course != null) ? parser.getCourseSlots() : parser.getLabSlots();
+        for(Slot s : slots){
+            newNode.setSlot(s)
+            if (!validator.validate(solution, newNode)){
+                continue;
+            }
+            if(toBeScheduled.size() == 0){
+                System.out.println("Depth first search found a solution");
+                return solution.add(n);
+            } else {
+                solution.add(n);
+                ArrayList<Node> temp = depthFirstSolve(solution, toBeScheduled)
+                if (temp.size() != 0){
+                    return temp;
+                }
+            }
+        }
+        // if hasn't returned by this point no solution in this branch
+        // return empty array list
+        return new ArrayList<Node>;
     }
-    */
+
+    private ArrayList<Node> breadthFirstSolve(ArrayList<Node> solution, ArrayList<ScheduledItem> toBeScheduled, currPenalty){
+        // basically don't use eval for this one, just want any solution
+
+        ScheduledItem current = toBeScheduled.get(0);
+        toBeScheduled.remove(0);
+
+
+        ArrayList<Slot> slots = (newNode.course != null) ? parser.getCourseSlots() : parser.getLabSlots();
+        ArrayList<Node> orderedBestChildren = new ArrayList<Node>;
+        int penalty;
+        for(Slot s : slots){
+            Node newNode = new Node(current, s, solution.get(solution.size()-1));
+            if (!validator.validate(solution, newNode)){
+                continue;
+            }
+            // will want to save this in the Node
+            penalty = evaluator.evaluate(solution, newNode);
+            if(currentPenalty + penalty >= minPenalty){
+                continue;
+            }
+            // TO-DO insert s into the right place on orderedBestSlots
+            // for now just add it
+            orderedBestChildren.add(newNode);
+        }
+
+        // loop through orderedBestSlots to next level in tree
+        for(Node n : orderedBestChildren){
+            ArrayList<Node> temp = solution;
+            temp.add(n)
+            if(toBeScheduled.size() == 0){
+                minPenalty = currentPenalty + evaluator.evaluate(solution, n);
+                break; // want to break on the best leaf, since no other leaf will be as good
+            } else {
+                temp = breadthFirstSolve(temp, toBeScheduled, currentPenalty + penalty)
+                return temp;
+            }
+        }
+        return new ArrayList<Node>;
+    }   
 }
