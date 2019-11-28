bd112ce887375a4422f75be68a2bb3012603e1a3
Kyle Perry <perry.246@gmail.com>
2019-11-27 23:41:20 -0700

    All hard constraint checks should be completed

--- src/searchproblem/Validator.java
+++ src/searchproblem/Validator.java
@@ -1,6 +1,7 @@
 package searchproblem;
 
 import searchproblem.classes.*;
+import java.util.*;
 
 public class Validator{
 	private final Parser parser;
@@ -15,47 +16,116 @@ public class Validator{
 		Slot assignedSlot = assignment.getSlot();
 		Course assignedCourse = assignment.getCourse();
 		Lab assignedLab = assignment.getLab();
+		ArrayList<ScheduledClass> conflicts = new ArrayList<>();
+		boolean isCourse813 = (assignedCourse != null && assignedCourse.getDepartment().equals("CPSC") && assignedCourse.getCourseNum() == 813);
+		boolean isCourse913 = (assignedCourse != null && assignedCourse.getDepartment().equals("CPSC") && assignedCourse.getCourseNum() == 913);
 
 		ScheduledClass currentClass;
 		ScheduledClass assignedClass;
-		
+
 		if(assignedCourse != null)
 			assignedClass = assignedCourse;
 		else
 			assignedClass = assignedLab;
 
+		if(assignedCourse != null && assignedSlot.getSlotTime() == 1100 && assignedSlot.getDay() == Day.TUESDAY)	{
+			return false;
+		}
 		if(assignedClass.getLectureNum()/10 == 9 && assignedSlot.getSlotTime() < 1800)
 			return false;
 
+		if(assignedClass.getUnwantedSlots().contains(assignedSlot))
+			return false;
+
+		if(isCourse813)
+		{
+			if(!(assignedSlot.getDay() == Day.TUESDAY && assignedSlot.getSlotTime() >= 1800))
+			{
+				return false;
+			}
+			for(Course c: parser.getCourses())
+			{
+				if(c.getDepartment() == "CPSC" && c.getCourseNum() == 313)
+					conflicts.add(c);
+			}
+			for(Lab l: parser.getLabs())
+			{
+				if(l.getDepartment() == "CPSC" && l.getCourseNum() == 313)
+					conflicts.add(l);
+			}
+		}
+
+		if(isCourse913)
+		{
+			if(!(assignedSlot.getDay() == Day.TUESDAY && assignedSlot.getSlotTime() >= 1800))
+			{
+				return false;
+			}
+			for(Course c: parser.getCourses())
+			{
+				if(c.getDepartment() == "CPSC" && c.getCourseNum() == 413)
+					conflicts.add(c);
+			}
+			for(Lab l: parser.getLabs())
+			{
+				if(l.getDepartment() == "CPSC" && l.getCourseNum() == 413)
+					conflicts.add(l);
+			}
+
+		}
 
 		while(current != null && (current.getCourse() != null || current.getLab() != null))
 		{
-			
-			if(this.isOverlap(current, assignment))
+
+			if(current.getCourse() != null && assignedCourse != null
+					&& current.getSlot() == assignedSlot 
+					&& current.getCourse().getCourseNum() / 100 == 5 && assignedCourse.getCourseNum() / 100 == 5 
+					&& current.getCourse().getDepartment().equals(assignedCourse.getDepartment())) {
+				return false;
+			}
+
+			if(isOverlap(current, assignment))
 				return false;
 
 			if(current.getSlot() == assignedSlot)
 				slotCount++;
 
-			if(current.getCourse() != null)
-			{
+			if(current.getCourse() != null)	{
 				currentClass = current.getCourse();
 			}
-			else 
-			{
+			else {
 				currentClass = current.getLab();
+			}
+
+			if(isCourse813)
+			{
+				if(current.getSlot().getSlotTime() == assignedSlot.getSlotTime() && current.getSlot().getDay() == Day.TUESDAY) {
+					if(currentClass.getDepartment() == "CPSC" && currentClass.getCourseNum() == 313)
+						return false;
+					for(ScheduledClass s: conflicts)
+						if(parser.areClassesIncompatible(currentClass, s))
+							return false;
+				}
+			}
+			else if(isCourse913)
+			{
+				if(current.getSlot().getSlotTime() == assignedSlot.getSlotTime() && current.getSlot().getDay() == Day.TUESDAY) {
+					if(currentClass.getDepartment() == "CPSC" && currentClass.getCourseNum() == 413) 
+						return false;
+					for(ScheduledClass s: conflicts)
+						if(parser.areClassesIncompatible(currentClass, s))
+							return false;
+				}
 			}
 
 			if( isSameDay(current, assignment)
 					&& current.getSlot().getSlotTime() == assignedSlot.getSlotTime()
-					&& ( parser.areClassesIncompatible(currentClass, assignedClass))){
-				{
+					&& ( parser.areClassesIncompatible(currentClass, assignedClass))){	
 				return false;
-			}
 			}
 			current = current.parent;
 
-			
+
 		}
 		if(slotCount > assignedSlot.getMaxCourses())
 			return false; 
@@ -63,23 +133,22 @@ public class Validator{
 		return true;
 	}
 
-	private boolean isOverlap(Node a, Node b)
-	{
+	private boolean isOverlap(Node a, Node b){
 
-		if(a.getLab() != null && b.getCourse() != null)
-		{
+		if(a.getLab() != null && b.getCourse() != null)	{
 			if(a.getLab().getDepartment().equals(b.getCourse().getDepartment()) 
 					&& (a.getLab().getCourseNum() == b.getCourse().getCourseNum())
 					&& (a.getSlot().getSlotTime() == b.getSlot().getSlotTime()))
 			{
 				if( (a.getSlot().getDay() == Day.MONDAY || a.getSlot().getDay() == Day.FRIDAY) && b.getSlot().getDay() == Day.MONDAY ||
-						(a.getSlot().getDay() == Day.TUESDAY ) && b.getSlot().getDay() == Day.TUESDAY) {
-					if(a.getSlot().getSlotTime() < getEndTime(b.getSlot(), true) || b.getSlot().getSlotTime() < getEndTime(a.getSlot(), false))
-					{
+						(a.getSlot().getDay() == Day.TUESDAY ) && b.getSlot().getDay() == Day.TUESDAY) 
+				{
+					if(a.getSlot().getSlotTime() < getEndTime(b.getSlot(), true) || b.getSlot().getSlotTime() < getEndTime(a.getSlot(), false)){
 						if(a.getLab().getLectureNum() == 0)
 							return true;
 						else if(a.getLab().getLectureNum() == b.getCourse().getLectureNum())
 							return true;
+
 					}	
 				}
 
